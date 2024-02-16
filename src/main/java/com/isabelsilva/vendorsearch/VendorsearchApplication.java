71020c9e4a236db7dab5f3889627b066ce866cb3
IsabelSiva <bell.cristina.pereiradasilva@gmail.com>
2024-02-16 15:27:24 -0300

    resolve git problems

---
+++ src/main/java/com/isabelsilva/vendorsearch/VendorsearchApplication.java
@@ -0,0 +1,13 @@
+package com.isabelsilva.vendorsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendorsearchApplication.class, args);
+	}
+
+}

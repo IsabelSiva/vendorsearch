763a2b964663f40e37d788109467cf932d35d166
IsabelSiva <bell.cristina.pereiradasilva@gmail.com>
2024-02-17 11:47:34 -0300

    add toString annotation at Vendor

--- src/main/java/com/isabelsilva/vendorsearch/entities/Vendor.java
+++ src/main/java/com/isabelsilva/vendorsearch/entities/Vendor.java
@@ -3,10 +3,12 @@ package com.isabelsilva.vendorsearch.entities;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@ToString
 public class Vendor {
     private Long id;
     private  String name;
